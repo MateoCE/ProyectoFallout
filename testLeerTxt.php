060496d0990e38477b8b24e6a9952c493da509f9
super <joel.jimenez.lorente@gmail.com>
2019-10-10 20:13:13 +0200

    Lista 6 palabras random

--- testLeerTxt.php
+++ testLeerTxt.php
@@ -14,16 +14,21 @@
 		
 		//384 caracteres en total, 354 solo especiales (30 de las palabras)
 		//for de 360
-
+		$palabrasRandom = [];
+		$contador=0;
+		while ($contador<6) {
+			$numAleatorio = rand(0,count($diccionario)-2);
+			if (!in_array($diccionario[$numAleatorio], $palabrasRandom)) {
+				array_push($palabrasRandom, $diccionario[$numAleatorio]);
+				$contador++;
+			}
+		}
+		print_r($palabrasRandom);
+		print_r(count($palabrasRandom));
+		
 		$simbolos = ["|", "º", "!", "ª", "@", "·", "$", "%", "&", "¬", "/", "(", ")", "=", "?", "'", "¿", "¡", "[", "]", "{", "}", "-", "<",">", "*", "+"];
 
-		
-
-
-
-
-
-
+		$stringPrincipal="";
 	?>
 </body>
 </html>
