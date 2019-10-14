7a9e7a50e31c232e45364d2322f62af6001f5c06
super <joel.jimenez.lorente@gmail.com>
2019-10-14 20:23:34 +0200

    Puntos

--- pruebasPantalla/scriptFallout.js
+++ pruebasPantalla/scriptFallout.js
@@ -6,38 +6,47 @@ function htmlCargado(){
 	segundoBloque = segundoBloque.replace(segundoBloque.substr(0,4), "", segundoBloque);
 	document.getElementById('blok').innerHTML = primerBloque;
 	document.getElementById('blok2').innerHTML = segundoBloque;
-
-
-	
 	
 	var contadorVidas= 4;
 	var Vidas=  "[] ".repeat(contadorVidas);
 	document.getElementById('intentos').innerHTML ="Attempts Remaining: "+ Vidas;
-
-
-
 }
-	var contadorVidas= 4;
 
+
+var contadorVidas= 4;
 function comprovarContrasena(palabra){
+		var primerBloque = document.getElementById('bloque1').innerHTML;
+		var segundoBloque = document.getElementById('bloque2').innerHTML;
 		var contrasenaScript = document.getElementById('contrasena').innerHTML;
 		if(palabra==contrasenaScript){
 			document.getElementById('bloqueFijo3').innerHTML+=">"+palabra+"<br/>"+">Correct password.";
 		}else{
+			console.log("asasas")
 			var contadorLetras=0;
 			for (var i = 0; i < palabra.length; i++) {
 				if (palabra[i]==contrasenaScript[i]) {
 					contadorLetras+=1;
 				}
 			}
-			document.getElementById('bloqueFijo3').innerHTML+=">"+palabra+"<br/>"+">Entry denied."+"<br/>";
-			document.getElementById('bloqueFijo3').innerHTML+=">Likeness="+contadorLetras+"<br/>";
-
+			document.getElementById('bloqueFijo3').innerHTML+=">"+palabra+"<br/>"+">Entry denied."+"<br/>"+">Likeness="+contadorLetras+"<br/>";
 			contadorVidas=contadorVidas-1;
 			var Vidas=  "[] ".repeat(contadorVidas);
 			document.getElementById('intentos').innerHTML = "Attempts Remaining: "+ Vidas;
 
-		}
+			//Puntos
+			var stringHTMLcompleto = document.getElementById(palabra);
+			var strSoloConBr = stringHTMLcompleto.innerHTML;
+			var strPuntos = "";
+			for (var i = 0; i < strSoloConBr.length; i++) {
+				if (strSoloConBr[i]=="<") {
+					strPuntos+="<br/>";
+					i+=3;
+				}else{
+					strPuntos+=".";
+				}
 
-		
+			}
+			console.log(strPuntos)
+			stringHTMLcompleto.innerHTML = strPuntos;
+		}
 	}
