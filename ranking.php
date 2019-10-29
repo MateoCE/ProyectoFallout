dbb82cae072e4b9c2d9f404fa444f1b129b792b8
Mateo Casas Echevarria <mcasasec@everis.com>
2019-10-29 11:21:01 +0100

    ranking sin php

--- ranking.php
+++ ranking.php
@@ -17,79 +17,7 @@
 		    <th>Time</th>
 		  </tr>
 
-		  <?php
-		  
-			$ranking = "resources/ranking/rankingEasy.txt";
-					  
-			if (isset($_GET["name"]) && isset($_GET["time"]) && isset($_GET["tries"]) && isset($_GET["gameMode" ]) {
-				$phpName = $_GET["name"];
-				$phpTime = $_GET["time"];
-				$phpTries = $_GET["tries"];
-				$phpGameMode = $_GET["gameMode"];
- 
-				$variables = $phpName . ";" . $phpTime . ";" . $phpTries . "\n";
-				
-				if($phpGameMode == "Easy"){
-					
-					file_put_contents("resources/ranking/rankingEasy.txt", $variables, FILE_APPEND | LOCK_EX);
-					$ranking = "resources/ranking/rankingEasy.txt";
-					
-				}elseif($phpGameMode == "Normal"){
-					
-					file_put_contents("resources/ranking/rankingNormal.txt", $variables, FILE_APPEND | LOCK_EX);
-					$ranking = "resources/ranking/rankingNormal.txt";
-					
-				}elseif($phpGameMode == "Hard"){
-				
-					file_put_contents("resources/ranking/rankingHard.txt", $variables, FILE_APPEND | LOCK_EX);
-					$ranking = "resources/ranking/rankingHard.txt";
-					
-				}
-			}
 
-			$arrayJugadores=[];
-			$file = fopen($ranking, "r") or exit("Unable to open file!");
-			while(!feof($file))
-			{
-				$jugador = trim(fgets($file));
-				$listaJugador = explode(';', $jugador);
-				if (count($listaJugador)>1) {
-					array_push($arrayJugadores, $listaJugador);
-				}	
-			}
-
-			uasort($arrayJugadores, 'sort_by_attempts');
-
-				function sort_by_attempts ($a, $b) {
-					if ($a[2]==$b[2]) {
-						return $a[1] - $b[1];
-					}else{
-						return $b[2] - $a[2];
-					}
-				}
-
-			$contador = 0;
-			foreach ($arrayJugadores as $row) {
-				if($contador<=10){
-					$contador++;
-					if($row[0]==$phpName && $row[1]==$phpTime && row[2]==$phpTries){
-						echo "<tr>";
-							echo "<td>$contador</td>";
-							echo "<td><span style='color:red'>$row[0]</span></td>";
-							echo "<td><span style='color:red'>$row[2]</span></td>";
-							echo "<td><span style='color:red'>$row[1]s</span></td>";
-						echo "<tr/>";
-					}else{	
-						echo "<tr>";
-							echo "<td>$contador</td>";
-							echo "<td>$row[0]</td>";
-							echo "<td>$row[2]</td>";
-							echo "<td>$row[1]s</td>";
-						echo "<tr/>";
-					}
-				}
-			}
-		?>
 		</table>
 		<div id="botones">
 			<button onclick="location.href='menuPrincipal.php'" class="letra boton">Menú principal</button>
