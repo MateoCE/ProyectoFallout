3a094d905230f68aab7517b4616675a90367c40e
Mateo Casas Echevarria <mcasasec@everis.com>
2019-10-29 11:23:07 +0100

    sin if en php

--- ranking.php
+++ ranking.php
@@ -16,7 +16,54 @@
 		    <th>Lives</th>
 		    <th>Time</th>
 		  </tr>
+		  <?php
+		  
+			$ranking = "resources/ranking/rankingEasy.txt";
 
+
+			$arrayJugadores=[];
+			$file = fopen($ranking, "r") or exit("Unable to open file!");
+			while(!feof($file))
+			{
+				$jugador = trim(fgets($file));
+				$listaJugador = explode(';', $jugador);
+				if (count($listaJugador)>1) {
+					array_push($arrayJugadores, $listaJugador);
+				}	
+			}
+
+			uasort($arrayJugadores, 'sort_by_attempts');
+
+				function sort_by_attempts ($a, $b) {
+					if ($a[2]==$b[2]) {
+						return $a[1] - $b[1];
+					}else{
+						return $b[2] - $a[2];
+					}
+				}
+
+			$contador = 0;
+			foreach ($arrayJugadores as $row) {
+				if($contador<=10){
+					$contador++;
+					if($row[0]==$phpName && $row[1]==$phpTime && row[2]==$phpTries){
+						echo "<tr>";
+							echo "<td>$contador</td>";
+							echo "<td><span style='color:red'>$row[0]</span></td>";
+							echo "<td><span style='color:red'>$row[2]</span></td>";
+							echo "<td><span style='color:red'>$row[1]s</span></td>";
+						echo "<tr/>";
+					}else{	
+						echo "<tr>";
+							echo "<td>$contador</td>";
+							echo "<td>$row[0]</td>";
+							echo "<td>$row[2]</td>";
+							echo "<td>$row[1]s</td>";
+						echo "<tr/>";
+					}
+				}
+			}
+		?>
 
 		</table>
 		<div id="botones">
